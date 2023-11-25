a3ba40abe59f9addbc906a24bb13b8b8120b3a20
kerembay9 <kerembay9@gmail.com>
2023-11-25 21:33:22 +0300

    integrating wallet

--- deploy.js
+++ deploy.js
@@ -3,16 +3,14 @@ const fs = require("fs-extra");
 require("dotenv").config();
 async function main() {
     const provider = new ethers.JsonRpcProvider(process.env.RPC_URL);
-    // const wallet = new ethers.Wallet(process.env.PRIVATE_KEY,provider);
-    const encryptedJson = fs.readFileSync("./.encryptedKey.json","utf-8")
-    let wallet = new ethers.Wallet.fromEncryptedJsonSync(encryptedJson, process.env.PRIVATE_KEY_PASSWORD)
-    wallet = await wallet.connect(provider);
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY,provider);
     const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi","utf-8");
     const binary = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.bin","utf-8");
     const contractFactory = new ethers.ContractFactory(abi,binary,wallet);
     console.log("Deploying, please wait");
     const contract = await contractFactory.deploy();
     await contract.deploymentTransaction().wait(1);
+    console.log(`Contract Address:${contract.target}`)
     const currentFavoriteNumber = await contract.retrieve();
     console.log(currentFavoriteNumber.toString());
     const transactionResponse = await contract.store("7");
